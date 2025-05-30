264c903ccd516e65d2484e858670b567dc18430c
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-05-30 17:52:33 +0300

    update Repository
    - add an overload taking an optional include lambda to let callers specify .Include(...) clauses for GetByIdAsync and ListAsync
    - add ExistsAsync(predicate) – a simple AnyAsync check for duplicates or existence tests
    - add Query(), which exposes an IQueryable<T> (with .AsNoTracking()) for things like ProjectTo<TDTO>() without re-fetching
    -

--- src/KSE.GameStore.DataAccess/Repositories/Repository.cs
+++ src/KSE.GameStore.DataAccess/Repositories/Repository.cs
@@ -16,21 +16,41 @@ public class Repository<T, TKey> : IRepository<T, TKey> where T : BaseEntity<TKe
         _dbSet = _context.Set<T>();
     }
     
-    public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
-    
-    public async Task<IEnumerable<T>> ListAsync(int pageNumber = 1, int pageSize = 10)
+    public async Task<T?> GetByIdAsync(
+        TKey id,
+        Func<IQueryable<T>, IQueryable<T>>? include = null)
     {
         IQueryable<T> query = _dbSet;
+        if (include != null)
+            query = include(query);
         
+        return await query.FirstOrDefaultAsync(e => e.Id!.Equals(id)!);
+    }
+    
+    public async Task<IEnumerable<T>> ListAsync(
+        int pageNumber = 1,
+        int pageSize   = 10,
+        Func<IQueryable<T>, IQueryable<T>>? include = null)
+    {
+        IQueryable<T> query = _dbSet;
+        if (include != null)
+            query = include(query);
+
         var skip = (pageNumber - 1) * pageSize;
         query = query.Skip(skip).Take(pageSize);
 
         return await query.ToListAsync();
     }
 
-    public async Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate, int pageNumber = 1, int pageSize = 10)
+    public async Task<IEnumerable<T>> ListAsync(
+        Expression<Func<T, bool>> predicate,
+        int pageNumber = 1,
+        int pageSize   = 10,
+        Func<IQueryable<T>, IQueryable<T>>? include = null)
     {
-        var query = _dbSet.Where(predicate);
+        IQueryable<T> query = _dbSet.Where(predicate);
+        if (include != null)
+            query = include(query);
 
         var skip = (pageNumber - 1) * pageSize;
         query = query.Skip(skip).Take(pageSize);
@@ -41,5 +61,7 @@ public class Repository<T, TKey> : IRepository<T, TKey> where T : BaseEntity<TKe
     public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
     public void Update(T entity) => _dbSet.Update(entity);
     public void Delete(T entity) => _dbSet.Remove(entity);
+    public Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate) => _dbSet.AnyAsync(predicate);
+    public IQueryable<T> Query() => _dbSet.AsNoTracking();
     public Task SaveChangesAsync() => _context.SaveChangesAsync();
 }
