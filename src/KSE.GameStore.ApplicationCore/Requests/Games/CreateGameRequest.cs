cb187cf61f137a10ff733749401b424da8c76280
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-05-30 17:42:39 +0300

    add CreateGameRequest and UpdateGameRequest models

---
+++ src/KSE.GameStore.ApplicationCore/Requests/Games/CreateGameRequest.cs
@@ -0,0 +1,16 @@
+namespace KSE.GameStore.ApplicationCore.Requests.Games;
+
+public record CreateGameRequest(
+    string Title,
+    string? Description,
+    int PublisherId,
+    IReadOnlyList<int> GenreIds,
+    IReadOnlyList<int> PlatformIds,
+    CreateGamePriceRequest Price,
+    IReadOnlyList<int>? RegionPermissionIds
+);
+
+public record CreateGamePriceRequest(
+    decimal Value,
+    int? Stock
+);
