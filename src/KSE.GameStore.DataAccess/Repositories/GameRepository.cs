ce270665ce45b534acf49bd9df547372f67d893e
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-06-02 21:02:38 +0300

    added GameRepository, removed redundant and changed incorrect checks

---
+++ src/KSE.GameStore.DataAccess/Repositories/GameRepository.cs
@@ -0,0 +1,54 @@
+using KSE.GameStore.ApplicationCore.Models;
+using KSE.GameStore.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KSE.GameStore.DataAccess.Repositories;
+
+public class GameRepository(GameStoreDbContext context) : Repository<Game, int>(context), IGameRepository
+{
+    public async Task<List<GameDTO>> GetGamesByPlatformAsync(int platformId)
+    {
+        var sql = @$"
+            SELECT g.id, g.title, g.description, g.publisher_id, g.created_at, g.updated_at
+            FROM games g
+            INNER JOIN game_platforms gp ON g.id = gp.game_id
+            WHERE gp.platform_id = {platformId}";
+
+        var games = await _context.Games
+            .FromSqlRaw(sql, platformId)
+            .Select(g => new GameDTO
+            {
+                Id = g.Id,
+                Title = g.Title,
+                Description = g.Description,
+                Publisher = g.Publisher.Name,
+                Genres = g.Genres.Select(genre => new GenreDTO
+                (
+                    genre.Id,
+                    genre.Name
+                )).ToList(),
+                Platforms = g.Platforms.Select(platform => new PlatformDTO
+                (
+                    platform.Id,
+                    platform.Name
+                )).ToList(),
+                Price = g.Prices.Select(price => new GamePriceDTO
+                (
+                    price.Id,
+                    price.Value,
+                    price.Stock
+                )).FirstOrDefault()!,
+                RegionPermissions = g.RegionPermissions != null
+                    ? g.RegionPermissions.Select(region => new RegionDTO
+                    (
+                        region.Id,
+                        region.Name,
+                        region.Code
+                    )).ToList()
+                    : new()
+            })
+            .ToListAsync();
+
+        return games;
+    }
+}
