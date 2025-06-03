ec06ae8a4c44a85d9d78d55c0f0d919a321e2af5
Mark Tabatskyi <mark.tabatskyi@gmail.com>
2025-06-03 15:21:01 +0300

    used IQueryable instead of raw sql

--- src/KSE.GameStore.DataAccess/Repositories/GameRepository.cs
+++ src/KSE.GameStore.DataAccess/Repositories/GameRepository.cs
@@ -8,47 +8,44 @@ public class GameRepository(GameStoreDbContext context) : Repository<Game, int>(
 {
     public async Task<List<GameDTO>> GetGamesByPlatformAsync(int platformId)
     {
-        var sql = @$"
-            SELECT g.id, g.title, g.description, g.publisher_id, g.created_at, g.updated_at
-            FROM games g
-            INNER JOIN game_platforms gp ON g.id = gp.game_id
-            WHERE gp.platform_id = {platformId}";
+        var query = from game in _dbSet
+                    from platform in game.Platforms
+                    where platform.Id == platformId
+                    select new GameDTO
+                    {
+                        Id = game.Id,
+                        Title = game.Title,
+                        Description = game.Description,
+                        Publisher = game.Publisher.Name,
+                        Genres = game.Genres.Select(genre => new GenreDTO
+                        (
+                            genre.Id,
+                            genre.Name
+                        )).ToList(),
+                        Platforms = game.Platforms.Select(platform => new PlatformDTO
+                        (
+                            platform.Id,
+                            platform.Name
+                        )).ToList(),
+                        Price = game.Prices
+                            .OrderByDescending(price => price.StartDate)
+                            .Select(price => new GamePriceDTO
+                            (
+                                price.Id,
+                                price.Value,
+                                price.Stock
+                            ))
+                            .FirstOrDefault()!,
+                        RegionPermissions = game.RegionPermissions != null
+                            ? game.RegionPermissions.Select(region => new RegionDTO
+                            (
+                                region.Id,
+                                region.Name,
+                                region.Code
+                            )).ToList()
+                            : new List<RegionDTO>()
+                    };
 
-        var games = await _context.Games
-            .FromSqlRaw(sql)
-            .Select(g => new GameDTO
-            {
-                Id = g.Id,
-                Title = g.Title,
-                Description = g.Description,
-                Publisher = g.Publisher.Name,
-                Genres = g.Genres.Select(genre => new GenreDTO
-                (
-                    genre.Id,
-                    genre.Name
-                )).ToList(),
-                Platforms = g.Platforms.Select(platform => new PlatformDTO
-                (
-                    platform.Id,
-                    platform.Name
-                )).ToList(),
-                Price = g.Prices.Select(price => new GamePriceDTO
-                (
-                    price.Id,
-                    price.Value,
-                    price.Stock
-                )).FirstOrDefault()!,
-                RegionPermissions = g.RegionPermissions != null
-                    ? g.RegionPermissions.Select(region => new RegionDTO
-                    (
-                        region.Id,
-                        region.Name,
-                        region.Code
-                    )).ToList()
-                    : new()
-            })
-            .ToListAsync();
-
-        return games;
+        return await query.ToListAsync();
     }
 }
